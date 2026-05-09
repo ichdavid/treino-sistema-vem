9391556073b1d506be47d8d3cbf3b239f0dc1ca7
ichdavid <davidbruno623@gmail.com>
2026-05-09 18:56:27 -0300

    criação e adição do metodo imprimir menu no main.

--- Sistema-vem/src/Main.java
+++ Sistema-vem/src/Main.java
@@ -1,3 +1,4 @@
+import com.sun.security.jgss.GSSUtil;
 import sistema.Usuario;
 
 import java.util.ArrayList;
@@ -6,12 +7,14 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
         List<Usuario> listUsuario = new ArrayList<>();
 
         int opcao;
 
         do {
+            imprimirMenu();
             opcao = sc.nextInt();
             sc.nextLine();
 
@@ -30,6 +33,22 @@ public class Main {
 
         } while (opcao != 0);
 
+    }
+
+    private static void imprimirMenu(){
+        System.out.println("============================");
+        System.out.println("         Cartão VAI         ");
+        System.out.println("============================");
+        System.out.println("1 - Cadastrar usuário       ");
+        System.out.println("2 - Cadastrar empresa       ");
+        System.out.println("3 - Cadastrar instituição   ");
+        System.out.println("4 - Solicitar cartão        ");
+        System.out.println("5 - perfil usuario          ");
+        System.out.println("6 - lista de cartões        ");
+        System.out.println("0 - sair                    ");
+        System.out.println("============================");
+        System.out.println();
+        System.out.print("Digite uma opção (0-6): ");
     }
 
 
